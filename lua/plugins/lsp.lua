0f9bc35e37448d0f66cc512310749f28e195ebd3
ngductoann <ngductoan0712@gmail.com>
2025-05-21 14:00:06 +0700

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -188,7 +188,7 @@ return {
       opts.root_dir = opts.root_dir
         or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
       opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.completion.luasnip,
+        -- nls.builtins.completion.luasnip,
         nls.builtins.formatting.fish_indent,
         nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
